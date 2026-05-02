fa427e824624cf7f8a68a9be376351ca9ed755b7
Danny Purdy <purdy.d.06@gmail.com>
2026-05-01 18:37:43 -0700

    Solved Solve Me First HR

---
+++ HR_SolveMeFirst.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  return a+b;
+}
+
+
+int main() {
+    // 1. Fast I/O: Makes cin and cout much faster
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // 2. Local File Redirect: 
+    // This only runs on your PC. It reads from your file instead of you typing.
+    // When you paste this into HackerRank, HackerRank ignores this part.
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int num1, num2; 
+    int sum; 
+    cin >> num1 >> num2;
+    sum = solveMeFirst(num1, num2);
+    cout << sum << endl;
+    return 0;
+}
